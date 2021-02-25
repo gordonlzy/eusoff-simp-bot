384eea8c743bf8600f60e269c7f0f609a0e9a71d
gordonlzy <61217117+gordonlzy@users.noreply.github.com>
2021-02-25 15:53:58 +0800

    change to tele

--- src/register.js
+++ src/register.js
@@ -70,7 +70,7 @@ function updateTeleHandle(userId, userTelegramHandle) {
   var user = userInfo(userId);
 
   // if user's telegram handle in the sheets is "fix" or empty, fill it in
-  if (user.telehandle === "fix" || user.telehandle.length === 0) {
+  if (user.tele === "fix" || user.tele.length === 0) {
     setUserTeleHandle(userId, userTelegramHandle);
   }
 }
