6ed6c3b8b486a354cfd4827683efb8fd1d6e84f2
gordonlzy <61217117+gordonlzy@users.noreply.github.com>
2021-02-26 02:41:30 +0800

    Add tutorial compatibility

--- src/register.js
+++ src/register.js
@@ -28,15 +28,17 @@ function register(userID) {
     sendText(userID, text);
 }
 
-function addUser(data) {  
+function addUser(data, userTelegramHandle) {  
     var raw_user_data = data.message.text;
     var user_data_arr = raw_user_data.split(" ");
 
     var name = user_data_arr[0];
     var room = user_data_arr[1];
     var id = data.message.chat.id;
+    var telegramhandle = "@" + userTelegramHandle;
+
   
-    newUser(id, name, room);
+    newUser(id, name, room, telegramhandle);
     addUserToTrack(id);
   
     var text =
@@ -50,16 +52,17 @@ function addUser(data) {
         name +
         '\n' +
         'Room: ' +
-        room +
-        '\n\n' +
-        "/profile - To check your profile details  \n\n" +
-        "/view - To view, take or simp for active requests  \n" + 
-        "/make_request - To make a request \n" + 
-        "/complete - To mark your request as complete \n" +
-        "/cancel - To delete your current requests that are not taken \n\n" +
-        "/leaderboard - To view the leaderboards \n\n" +
-        "/subscribe - To get notified of new favours \n" + 
-        "/unsubscribe - To unsubscribe from updates \n";
+        room;
   
       sendText(id, text);
+      sendTutorial(id);
+}
+
+function updateTeleHandle(userId, userTelegramHandle) {
+  var user = userInfo(userId);
+
+  // if user's telegram handle in the sheets is "fix" or empty, fill it in
+  if (user.tele === "fix" || user.tele.length === 0) {
+    setUserTeleHandle(userId, userTelegramHandle);
+  }
 }
